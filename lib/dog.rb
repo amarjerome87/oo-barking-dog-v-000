e5238e8448b4eb16070e2c34593fd6d16d8a3cbe
Amar Chatman <amarjerome@gmail.com>
2017-12-19 19:16:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,17 @@
-class Dog 
+class Dog
   def initialize(name,bark)
-    @name = name 
-  end 
+    @name = name
+  end
 
-  def bark 
+  def bark
     puts "Woof!"
-  end 
+  end
 
-  def name 
-    @name 
-  end 
+  def name
+    @name
+  end
 
   def name=(new_name)
     @name = new_name
-  end 
-end 
-
+  end
+end
